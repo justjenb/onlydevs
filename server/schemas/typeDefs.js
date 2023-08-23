bd234dc122343006461b1a5f1c06452c9bf12b85
Jen B. <jenbutler984@gmail.com>
2023-08-23 19:02:43 -0400

    Fixed things

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,38 +1,60 @@
 const typeDefs = `
   type User {
     _id: ID!
-    username: String
-    email: String
-    password: String
-    posts: INT
+    username: String!
+    email: String!
+    posts: Int
+    savedBooks: [Book]  # Assuming saved books return a list of Book objects
   }
-  type Post {
-    user: String!
-    description: String!
-    postId: String!
+
+  type Book {
+    _id: ID!
+    title: String!
+    authors: [String]!
+    description: String
+    image: String
     link: String
-    title: String
-    likes: INT
   }
+
   type Tag {
     _id: ID!
     name: String!
     description: String
   }
 
-type Auth {
-  token: String!
-  user: User!
-}
+  type Auth {
+    token: String!
+    user: User!
+  }
 
-type Mutation {
-  login(email: String!, password: String!): Auth!
-  addUser(username: String!, email: String!, password: String!): Auth!
-}
+  input NewUserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
 
-type Query {
-  me: User
-}
+  input SaveBookInput {
+    bookId: ID!
+  }
+
+  input CreateTagInput {
+    name: String!
+    description: String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth!
+    addNewUser(input: NewUserInput!): Auth!
+    addSavedBook(input: SaveBookInput!): User
+    removeSavedBook(bookId: ID!): User
+    createTag(input: CreateTagInput!): Tag
+  }
+
+  type Query {
+    me: User
+    getAllTags: [Tag]
+    getTagById(id: ID!): Tag
+  }
 `;
 
 module.exports = typeDefs;
