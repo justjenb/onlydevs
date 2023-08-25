abd356f55bbca9a7aba577da2406b46f9869a5db
Aidan <farinaaidan2@gmail.com>
2023-08-24 21:48:47 -0400

    working on navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Container, Modal, Button } from "react-bootstrap";
+import { Navbar, Nav, Container, Modal, Button, Form, FormControl, ListGroup } from "react-bootstrap";
 import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
-
 import Auth from "../utils/auth";
+import { useQuery } from '@apollo/client';
+import { GET_ALL_TAGS } from '../utils/queries';
 
 const AppNavbar = () => {
   const [showModal, setShowModal] = useState(false);
-  const [showSignup, setShowSignup] = useState(false); // New state
+  const [showSignup, setShowSignup] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [focusedSuggestionIndex, setFocusedSuggestionIndex] = useState(-1);
+
+  const { loading, data } = useQuery(GET_ALL_TAGS);
+console.log("Loading:", loading);
+console.log("Data:", data);
+  let allPossibleSuggestions = data?.getAllTags || [];
+
+  if (data && data.getAllTags) {
+    allPossibleSuggestions = data.getAllTags.map(tag => tag.name);
+  }
+  useEffect(() => {
+    setFocusedSuggestionIndex(-1);
+  }, [suggestions]);
+
+  const handleSearchChange = (e) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    const newSuggestions = allPossibleSuggestions.filter(suggestion =>
+      suggestion.toLowerCase().includes(value.toLowerCase())
+    );
+    setSuggestions(newSuggestions);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Tab" && !e.shiftKey) {
+      e.preventDefault();
+      const nextIndex = (focusedSuggestionIndex + 1) % suggestions.length;
+      setFocusedSuggestionIndex(nextIndex);
+    } else if (e.key === "Tab" && e.shiftKey) {
+      e.preventDefault();
+      const nextIndex = (focusedSuggestionIndex - 1 + suggestions.length) % suggestions.length;
+      setFocusedSuggestionIndex(nextIndex);
+    } else if (e.key === "Enter" && focusedSuggestionIndex !== -1) {
+      handleSuggestionClick(suggestions[focusedSuggestionIndex]);
+    }
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    setSearchTerm(suggestion);
+    setSuggestions([]);
+  };
 
   return (
     <>
@@ -17,11 +61,43 @@ const AppNavbar = () => {
           <Navbar.Brand as={Link} to="/">
             OnlyDevs
           </Navbar.Brand>
+
+          <Form inline className="mr-auto" onKeyDown={handleKeyDown}>
+            {/* Common parent div */}
+            <div className="position-relative">
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+                value={searchTerm}
+                onChange={handleSearchChange}
+              />
+              <Button variant="outline-success">Search</Button>
+
+              {/* Suggestions */}
+              {suggestions.length > 0 && (
+                <ListGroup className="position-absolute w-100 suggestion-list">
+                  {suggestions.map((suggestion, index) => (
+                    <ListGroup.Item 
+                      key={index} 
+                      onClick={() => handleSuggestionClick(suggestion)}
+                      className={index === focusedSuggestionIndex ? 'active' : ''}
+                      tabIndex={0}
+                    >
+                      {suggestion}
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </div>
+          </Form>
+
           <Navbar.Toggle aria-controls="navbar" />
           <Navbar.Collapse id="navbar">
             <Nav className="ml-auto">
               <Nav.Link as={Link} to="/feeds">
-Feeds              </Nav.Link>
+                Feeds
+              </Nav.Link>
               {Auth.loggedIn() ? (
                 <>
                   <Nav.Link as={Link} to="/saved">
