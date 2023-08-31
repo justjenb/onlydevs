4b159f6ac1c89372821277406a9a0f18d7ce4ad7
slayonce <katharinemg@gmail.com>
2023-08-30 22:57:29 -0400

    reverted home

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,41 +1,25 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Layout, Typography } from "antd";
-const { Header, Content } = Layout;
-const { Text } = Typography;
+import { Button, Avatar, Row, Col, Layout, Typography } from "antd";
 import { LogoutOutlined } from "@ant-design/icons";
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_POSTS } from "../../utils/queries";
 import { UPDATE_LIKES } from '../../utils/mutations';
 import PostList from "../../components/PostList/index";
 import { useSearch } from '../../context/SearchContext';
-import { Grid } from "@mui/material";
-
-
-
+import { Container, Box } from '@mui/material';
+const { Header, Content } = Layout;
+const { Text } = Typography;
 const Home = () => {
   const [userDataGithub, setUserDataGithub] = useState(null);
   const [userDataGoogle, setUserDataGoogle] = useState(null);
   const [allPosts, setAllPosts] = useState([]);
+  const { performSearch } = useSearch();
   const { searchResults } = useSearch();
   const { loading, error, data: queryData } = useQuery(QUERY_POSTS);
   const [updateLikes] = useMutation(UPDATE_LIKES);
   const loginWith = useRef(localStorage.getItem("loginWith"));
   const navigate = useNavigate();
-
-
-  useEffect(() => {
-    if (queryData && queryData.posts) {
-      setAllPosts(queryData.posts);
-    }
-  }, [queryData]);
-
-  useEffect(() => {
-    if (searchResults.length > 0) {
-      setAllPosts(searchResults);
-    }
-  }, [searchResults]);
-
   useEffect(() => {
     if (queryData && queryData.posts) {
       setAllPosts(queryData.posts);
@@ -49,7 +33,6 @@ const Home = () => {
       setAllPosts(searchResults);
     }
   }, [searchResults]);
-  
   const handleLike = async (postId) => {
     try {
       const { data: mutationData } = await updateLikes({
@@ -65,33 +48,44 @@ const Home = () => {
     localStorage.removeItem("loginWith");
     navigate("/");
   };
-
-
-// console.log("Posts" , allPosts)
-// If user data is not available, show a log in message
-if (!userDataGithub && !userDataGoogle) {
-return (
-  <Grid container rowSpacing={2}>
-    <Grid item xs={12} className="main-content">
-      {loading && <div>Loading...</div>}
-      {error && <div>Error: {error.message}</div>}
-      <PostList posts={allPosts} title="Recent Posts" />
-    </Grid>
-</Grid>
-// </Content>
-);
-}
-// If user data is available, show user details
-return (
-<Grid container rowSpacing={2}>
-  <Grid item xs={12} className="main-content">
-    <div>Hello</div>
-    <PostList posts={allPosts} title="Recent Posts" />
-  </Grid>
-</Grid>
-);
-
-
+  if (!userDataGithub && !userDataGoogle) {
+    return (
+      <Layout>
+        <Content style={{ padding: "20px", textAlign: "center" }}>
+          <Row justify="center">
+            <Col>
+              <Text strong>Welcome to OnlyDevs!</Text>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              {loading && <div>Loading...</div>}
+              {error && <div>Error: {error.message}</div>}
+              <PostList posts={allPosts} title="Recent Posts" />
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
+    );
+  }
+  return (
+    <Container fixed>
+      <Box style={{ height: '100vh' }}>
+        <Avatar
+          size="large"
+          src={
+            loginWith.current === "GitHub"
+              ? userDataGithub?.avatar_url
+              : userDataGoogle?.picture
+          }
+        />
+        <Button type="primary" icon={<LogoutOutlined />} onClick={setLogOut}>
+          Log out
+        </Button>
+        <div>Hello</div>
+        <PostList posts={allPosts} title="Recent Posts" />
+      </Box>
+    </Container>
+  );
 };
-
 export default Home;
