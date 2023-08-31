806ceef12d31786f440a0be89968b8ffe4e0dee5
slayonce <katharinemg@gmail.com>
2023-08-30 22:25:18 -0400

    fixed home back to Aiden

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -11,11 +11,6 @@ import PostList from "../../components/PostList/index";
 import { useSearch } from '../../context/SearchContext';
 import { Grid } from "@mui/material";
 
-import {
-getAccessTokenGithub,
-getUserDataGithub,
-getUserDataGoogle,
-} from "./services/home-services";
 
 
 const Home = () => {
@@ -27,6 +22,7 @@ const Home = () => {
   const [updateLikes] = useMutation(UPDATE_LIKES);
   const loginWith = useRef(localStorage.getItem("loginWith"));
   const navigate = useNavigate();
+
 
   useEffect(() => {
     if (queryData && queryData.posts) {
@@ -41,63 +37,23 @@ const Home = () => {
   }, [searchResults]);
 
 
-const handleLike = async (postId) => {
-try {
-// console.log("Post ID:", postId)
-const { data: mutationData } = await updateLikes({
-variables: { postId }
-});
-console.log('Updated likes:', mutationData);
-} catch (err) {
-console.error('Error updating likes:', err);
-}
-};
+  const handleLike = async (postId) => {
+    try {
+      const { data: mutationData } = await updateLikes({
+        variables: { postId }
+      });
+      console.log('Updated likes:', mutationData);
+    } catch (err) {
+      console.error('Error updating likes:', err);
+    }
+  };
 
+  const setLogOut = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("loginWith");
+    navigate("/");
+  };
 
-const loginWith = useRef(localStorage.getItem("loginWith"));
-const navigate = useNavigate();
-
-
-useEffect(() => {
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const codeParam = urlParams.get("code");
-const accessToken = localStorage.getItem("accessToken");
-
-
-if (codeParam && !accessToken && loginWith.current === "GitHub") {
-getAccessTokenGithub(codeParam).then((resp) => {
-localStorage.setItem("accessToken", resp.access_token);
-getUserDataGithub(resp.access_token).then((resp) => {
-setUserDataGithub(resp);
-});
-});
-} else if (codeParam && accessToken && loginWith.current === "GitHub") {
-getUserDataGithub(accessToken).then((resp) => {
-localStorage.setItem("accessToken", accessToken);
-setUserDataGithub(resp);
-});
-}
-}, [loginWith]);
-
-
-useEffect(() => {
-const accessToken = localStorage.getItem("accessToken");
-
-
-if (accessToken && loginWith.current === "Google") {
-getUserDataGoogle(accessToken).then((resp) => {
-setUserDataGoogle(resp);
-});
-}
-}, [loginWith]);
-
-
-const setLogOut = () => {
-localStorage.removeItem("accessToken");
-localStorage.removeItem("loginWith");
-navigate("/");
-};
 // console.log("Posts" , allPosts)
 // If user data is not available, show a log in message
 if (!userDataGithub && !userDataGoogle) {
@@ -106,69 +62,22 @@ return (
     <Grid item xs={12} className="main-content">
       {loading && <div>Loading...</div>}
       {error && <div>Error: {error.message}</div>}
-      {allPosts.length > 0 && (
-        <div>
-          <h3>Posts</h3>
-          <ul>
-            {allPosts.map((post, index) => (
-            // console.log("Current post object:", post),
-            <li key={index}>
-            <strong>Title:</strong> {post.title} <br />
-            <strong>Description:</strong> {post.description} <br />
-            <button onClick={() => handleLike(post._id)}>Like</button>
-            </li>
-            ))}
-          </ul>
-        </div>
-        )}  
+      <PostList posts={allPosts} title="Recent Posts" />
     </Grid>
 </Grid>
 // </Content>
 );
 }
-
 // If user data is available, show user details
 return (
 <Grid container rowSpacing={2}>
   <Grid item xs={12} className="main-content">
     <div>Hello</div>
-      <PostList posts={allPosts} searchResults={searchResults} title="Recent Posts"/>
-    </Grid>
+    <PostList posts={allPosts} title="Recent Posts" />
+  </Grid>
 </Grid>
 );
 
-
-// If user data is available, show user details
-// return (
-// <Layout>
-// <AppNavbar />
-// <Content style={{ padding: "20px", textAlign: "center" }}>
-// <Header
-// style={{
-// display: "flex",
-// alignItems: "center",
-// justifyContent: "space-between",
-
-
-// }}
-// >
-// <Avatar
-// size="large"
-// src={
-// loginWith.current === "GitHub"
-// ? userDataGithub?.avatar_url
-// : userDataGoogle?.picture
-// }
-// />
-// <Button type="primary" icon={<LogoutOutlined />} onClick={setLogOut}>
-// Log out
-// </Button>
-// </Header>
-// <div>Hello</div>
-// <PostList posts={allPosts} searchResults={searchResults} title="Recent Posts"/>
-// </Content>
-// </Layout>
-// );
 
 };
 
